b40173f2da0ea28eeee7b4e9d81193424d523887
Kevin Kang <kevin97kang@gmail.com>
2014-07-12 08:52:46 -0700

    committed just to save progress. tiny updates.

---
+++ js/googlemap.js
@@ -0,0 +1,31 @@
+﻿function initialize() {
+    var myLatlng = new google.maps.LatLng(37.404163, -122.035285);
+    var mapOptions = {
+        zoom: 4,
+        center: myLatlng
+    };
+
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    var contentString = '<div id="content">' +
+        '<div id="siteNotice">' +
+        '</div>' +
+        '<h1 id="firstHeading" class="firstHeading">Microsoft: Sunnyvale Campus</h1>'+
+        '</div>' +
+        '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'Microsoft: Sunnyvale Campus'
+    });
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.open(map, marker);
+    });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
